9d8d9b5ccae4aed5981ada90afa94e6198fea4cc
nikita <nega123497@mail.com>
2022-08-28 14:46:01 +0300

    feat: finished

---
+++ src/data/difficulties.js
@@ -0,0 +1,24 @@
+const difficulties = [
+  {
+    id: 'veryeasy',
+    name: 'Очень легкий'
+  },
+  {
+    id: 'easy',
+    name: 'Легкий'
+  },
+  {
+    id: 'normal',
+    name: 'Средний'
+  },
+  {
+    id: 'hard',
+    name: 'Высокий'
+  },
+  {
+    id: 'veryhard',
+    name: 'Очень высокий'
+  },
+]
+
+export default difficulties
